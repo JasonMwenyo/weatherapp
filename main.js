6ead701aa0059412852a23f6ffa63fcc37aa77b3
Jason Mwenyo <jasonmwenyo@gmail.com>
2021-09-10 15:17:46 +0200

    Get temperature API response

--- main.js
+++ main.js
@@ -20,20 +20,35 @@ addEventListener("keydown", (e) =>{
 });
 
 function searchWeather(q){
+    let loc;
     fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?q=${q}&apikey=${apiKey}`) 
     .then(response => response.json())
-    // .then(data => console.log(data));
-    .then(data => {
-        let output = "<h3>Posts</h3>";
-        data.forEach(function(post){
-            output += ` 
-                <div>
-                    <p>${post.Key}</p>
-                </div>
-            `;
-        });
-        document.getElementById("temp").innerHTML = output;
-    })
+    // .then(data => console.log(data))
+    .then(data => { return loc = data[0].Key})
+    .then(loc => getTemperature(loc))
+    
+    // .then(console.log(loc))
+    
+    // .then(data => {
+    //     let output = "<h3>Posts</h3>";
+    //     data[0].forEach(function(post){
+    //         output += ` 
+    //             <div>
+    //                 <p>${post.Key}</p>
+    //             </div>
+    //         `;
+    //     });
+    //     document.getElementById("temp").innerHTML = output;
+    // });
+}
+
+// 243939
+
+function getTemperature(loc){
+    // console.log(loc)
+    fetch(`http://dataservice.accuweather.com/currentconditions/v1/${loc}?apikey=${apiKey}`)
+    .then(response => response.json())
+    .then(data => console.log(data[0]))
 }
 
 
