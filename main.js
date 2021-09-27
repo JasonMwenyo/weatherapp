c88b7b9bb6a0ee58697858282913f0a1247d6e08
Jason Mwenyo <Jason Mwenyo>
2021-09-27 07:26:49 +0200

    GetWeatherText function fixed

--- main.js
+++ main.js
@@ -8,21 +8,20 @@ addEventListener("keydown", (e) =>{
     if(e.key === "Enter"){
         if(cityName.value.match(letters)){
             q = cityName.value;
-            searchWeather(q);
+            getLocation(q);
         } else {
             console.log("Wrong");
         }
     }
 });
 
-function searchWeather(q){
+function getLocation(q){
     let locationKey;
     fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?q=${q}&apikey=${apiKey}`) 
     .then(response => response.json())
-    // .then(data => console.log(data))
     .then(data => { return locationKey = data[0].Key})
-    .then(locationKey => getTemperature(locationKey))   
-    .then(locationKey => console.log(locationKey))    
+    .then(locationKey => getTemperature(locationKey))
+ 
 
     // .then(console.log(loc))
     
@@ -37,31 +36,33 @@ function searchWeather(q){
     //     });
     //     document.getElementById("temp").innerHTML = output;
     // });
-}
+
 
 // 243939
+    // .then(getTemperature(locationKey))
 
-function getTemperature(locationKey){
-    let temp;
-    fetch(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`)
-    .then(response => response.json())
-    .then(data => temp = (data[0].Temperature.Metric.Value + "°" + data[0].Temperature.Metric.Unit))
-    .then(() => console.log(temp))
 }
-
-
-// Fix this function
-function getWeatherText(l){
-    // console.log(l)
-    // let condition;
-    // fetch(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`)
-    // .then(response => response.json())
-    // .then(data => console.log(data))
-}
-
-
-
 
 // Windhoek
 
 
+function getTemperature(locationKey){
+    let temp;
+    let weatherText = locationKey; //Assign value of location to variable
+    fetch(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`)
+    .then(response => response.json())
+    .then(data => temp = (data[0].Temperature.Metric.Value + "°" + data[0].Temperature.Metric.Unit))
+    .then(() => console.log(temp))
+    .then(getWeatherText(weatherText))
+   
+}
+
+
+// Fix this function
+function getWeatherText(weatherText){
+    let weather;
+    fetch(`http://dataservice.accuweather.com/currentconditions/v1/${weatherText}?apikey=${apiKey}`)
+    .then(response => response.json())
+    .then(data => weather = (data[0].WeatherText))
+    .then(() => console.log(weather))
+}
