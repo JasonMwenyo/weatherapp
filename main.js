90cd213ee89e942eb55abe96dcfbbe6bcb8224ef
Jason Mwenyo <jasonmwenyo@gmail.com>
2021-10-04 22:54:06 +0200

    Update City and Temperature DOM successfully

--- main.js
+++ main.js
@@ -8,60 +8,59 @@ addEventListener("keydown", (e) =>{
     if(e.key === "Enter"){
         if(cityName.value.match(letters)){
             q = cityName.value;
-            searchWeather(q);
+            cityNameDisplay(q);
+            searchTempKey(q);
+            searchWeatherKey(q);
         } else {
             console.log("Wrong");
         }
     }
 });
 
-function searchWeather(q){
+// Displays City name in DOM
+function cityNameDisplay(q){
+    document.getElementById("city").innerHTML = q;
+}
+
+// This function pulls the City key
+function searchTempKey(q){
     let locationKey;
     fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?q=${q}&apikey=${apiKey}`) 
     .then(response => response.json())
-    // .then(data => console.log(data))
     .then(data => { return locationKey = data[0].Key})
-    .then(locationKey => getTemperature(locationKey))   
-    .then(locationKey => console.log(locationKey))    
-
-    // .then(console.log(loc))
-    
-    // .then(data => {
-    //     let output = "<h3>Posts</h3>";
-    //     data[0].forEach(function(post){
-    //         output += ` 
-    //             <div>
-    //                 <p>${post.Key}</p>
-    //             </div>
-    //         `;
-    //     });
-    //     document.getElementById("temp").innerHTML = output;
-    // });
+    .then(locationKey => getTemperature(locationKey))      
 }
 
-// 243939
-
+// Pulls the temperature
 function getTemperature(locationKey){
     let temp;
     fetch(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`)
     .then(response => response.json())
     .then(data => temp = (data[0].Temperature.Metric.Value + "°" + data[0].Temperature.Metric.Unit))
-    .then(() => console.log(temp))
+    .then((temp) => document.getElementById("temperature").innerHTML = temp);
 }
 
 
-// Fix this function
-function getWeatherText(l){
-    // console.log(l)
-    // let condition;
-    // fetch(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`)
-    // .then(response => response.json())
-    // .then(data => console.log(data))
+// Pulls city key for weather function
+function searchWeatherKey(q){
+    let locationKey;
+    fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?q=${q}&apikey=${apiKey}`) 
+    .then(response => response.json())
+    .then(data => { return locationKey = data[0].Key})
+    .then(locationKey => getWeatherText(locationKey))      
+}
+
+// Pulls current weather condition eg. Clear / Sunny
+function getWeatherText(locationKey){
+    let weatherText;
+    fetch(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`)
+    .then(response => response.json())
+    .then(data => weatherText = (data[0].WeatherText))
+    .then(() => console.log(weatherText))
 }
 
 
 
 
-// Windhoek
 
 
